e72ee95047ed9c01e3396c36bcd85ea0449bb5ec
talank <talank@gces.edu.np>
2020-03-10 16:48:46 +0545

    developing api

--- login.php
+++ login.php
@@ -1,39 +1,24 @@
 <?php
-	if(isset($_GET['username_not_found'])==true) {
-		echo '
-		
-			<div id = "loginError1">incorrect username or password</div>
-		';
-		// <script type = "text/javascript">
-			// 	// var err = document.getElementById("loginError1");
-			// 	// err.style.color = "red";
-			// 	// err.innerHTML("Username Not Found");
-			// 	alert("Username Not found");
-			// </script>
-	}
-	else if(isset($_GET['password_wrong'])==true) {
-		echo '
-		<div id = "loginError1">incorrect username or password</div>
-		';
-		// <script type = "text/javascript">
-			// 	// var err = document.getElementById("loginError2");
-			// 	// err.style.color = "red";
-			// 	// err.innerHTML("Invalid Username or Password");
-			// 	alert("Invalid Username or Password");
-			// </script>
+    $errorMsg1= "";
+	if(isset($_GET['username_not_found'])==true || isset($_GET['password_wrong'])==true) {
+	    $errorMsg = "Incorrect username or password";
 	}
 	else{
-		// echo 'Valid';
+        $errorMsg = "";
 	}
-?>	
+?>
 <div>
 	<div class = loginbox>
 		<img src="images/logo.PNG" class = "head-logo">
 		<img src="images/user.png" class = "user">
 		<h3>Sign In Here</h3>
 		<form class="loginForm" method="post" action="process/login_process.php">
-			<div id = "loginError1"></div>
-			<div id = "loginError2"></div>
+            <?php
+                echo"
+                <div id = \"loginError\" style='color: red; text-align: center'>$errorMsg</div>
+                ";
+            ?>
+
 			<div class="inputbox">
 				<span><i class="fa fa-user" aria-hidden="true"></i></span>
 				<input type="text" name="username" placeholder="Enter your Username" required>
@@ -43,7 +28,7 @@
 				<input type="password" name="password" placeholder="Enter your Password" required>
 			</div>
 				<input type="submit" name="login" value="Sign In"><br>
-				<a class = "fpass" href="forgot_password.php">Forgot Password?</a>	
+				<a class = "fpass" href="forgot_password.php">Forgot Password?</a>
 		</form>
 
 		<div class = login_foot>
