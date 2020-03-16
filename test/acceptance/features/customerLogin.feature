8a3ede6af42f262ac0d54b0f311ccc95ffb4133a
talank <talank@gces.edu.np>
2020-03-16 16:56:25 +0545

    SQA Workshop day-2 demo added

--- test/acceptance/features/customerLogin.feature
+++ test/acceptance/features/customerLogin.feature
@@ -3,16 +3,20 @@ Feature: customer login
     I want to login to chamena-griha
     So that I can use the chamena-griha services
 
-    Background:
-        Given user has created an account in chamena-griha with username "zombie123" and password "password"
-        And user has browsed to the login page
+#    Background:
+#        Given user has created an account in chamena-griha with username "zombie123" and password "password"
+#        And user has browsed to the login page
 
     Scenario: customer login with correct username and password
+        Given user has created an account in chamena-griha with username "zombie123" and password "password"
+        And user has browsed to the login page
         When user enters username "zombie123" and password "password" in the login form
         And user tries to login
         Then user should be redirected to the customer home page
 
     Scenario Outline: login with wrong credentials
+        Given user has created an account in chamena-griha with username "zombie123" and password "password"
+        And user has browsed to the login page
         When user enters username "<username>" and password "<password>" in the login form
         And user tries to login
         Then an error message "<message>" should be shown in the screen
--- test/acceptance/stepDefinitions/customerLoginContext.js
+++ test/acceptance/stepDefinitions/customerLoginContext.js
@@ -7,22 +7,40 @@ Given('user has created an account in chamena-griha with username {string} and p
 });
 
 Given('user has browsed to the login page', function () {
-    return client.page.indexPage().navigate()
+    return client.url(client.launch_url + '/index.php');
+    // return client.page.indexPage().navigate()
 });
 
 When('user enters username {string} and password {string} in the login form', function (username, password) {
-   return client.page.indexPage().userEntersUsernamePassword(username, password)
+    const loginForm = '.loginForm'
+    const usernameField = '//div/input[@name="username"]'
+    const passwordField = '//div/input[@name="password"]'
+
+    return client.waitForElementVisible(loginForm)
+        .setValue('xpath', usernameField, username)
+        .setValue('xpath', passwordField, password);
+    // return client.page.indexPage().userEntersUsernamePassword(username, password)
 });
 
 When('user tries to login', function () {
-    return client.page.indexPage().userTriesToLogin();
+    const loginButton = '//div/form/input'
+
+    return client.useXpath().waitForElementVisible(loginButton)
+        .click('xpath', loginButton).useCss();
+    // return client.page.indexPage().userTriesToLogin();
 });
 
 Then('user should be redirected to the customer home page', function () {
-    return client.page.indexPage().redirectToHomePage();
+    return client.waitForElementVisible('.slider-image')
+    // return client.page.indexPage().redirectToHomePage();
 });
 
 Then('an error message {string} should be shown in the screen', function (errorMessage) {
-    return client.page.indexPage().isLoginErrorMsgVisible(errorMessage);
+    return client.waitForElementVisible('#loginError').element('css selector', '#loginError', (result) =>{
+        client.elementIdText(result.value['ELEMENT'], (result) => {
+            assert.strictEqual(result.value, errorMessage, `Expected ${errorMessage} but got ${result.value}`)
+        })
+    })
+    // return client.page.indexPage().isLoginErrorMsgVisible(errorMessage);
 });
 
